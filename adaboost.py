3722efc7b791055f10d3a4e729dedf55f61591ba
luuuyi <435977170@qq.com>
2017-04-06 16:11:46 +0800

    luyi at DSP upload in 2017/04/06

--- adaboost.py
+++ adaboost.py
@@ -25,10 +25,10 @@ def buildStump(data_arr,labels,D):
             for inequel in ['lt','gt']:
                 thresh_val = (range_min + float(j) * step_size)
                 predict_val = stumpClassify(data_mat,i,thresh_val,inequel)
-                error_array = mat(ones(m,1))
+                error_array = mat(ones((m,1)))
                 error_array[predict_val == labels_mat] = 0
                 weight_error = D.T * error_array
-                #print
+                print "split: dim %d, thresh %.2f, thresh ineqal：%s, the weighted error is %.3f" % (i,thresh_val,inequel,weight_error)
                 if weight_error < min_error:
                     min_error = weight_error
                     best_class_est = predict_val.copy()
@@ -37,4 +37,33 @@ def buildStump(data_arr,labels,D):
                     best_stump['ineq'] = inequel
     return best_stump, min_error, best_class_est
 
+def adaBoostTrainDS(dataArr,classLabels,numIt=40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m,1))/m)   #init D to all equal
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(numIt):
+        bestStump,error,classEst = buildStump(dataArr,classLabels,D)#build Stump
+        #print "D:",D.T
+        alpha = float(0.5*log((1.0-error)/max(error,1e-16)))#calc alpha, throw in max(error,eps) to account for error=0
+        bestStump['alpha'] = alpha  
+        weakClassArr.append(bestStump)                  #store Stump Params in Array
+        #print "classEst: ",classEst.T
+        expon = multiply(-1*alpha*mat(classLabels).T,classEst) #exponent for D calc, getting messy
+        D = multiply(D,exp(expon))                              #Calc New D for next iteration
+        D = D/D.sum()
+        #calc training error of all classifiers, if this is 0 quit for loop early (use break)
+        aggClassEst += alpha*classEst
+        #print "aggClassEst: ",aggClassEst.T
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T,ones((m,1)))
+        errorRate = aggErrors.sum()/m
+        print "total error: ",errorRate
+        if errorRate == 0.0: break
+    return weakClassArr,aggClassEst
+
 if __name__ == '__main__':
+    datas_mat, labels = loadSimpleData()
+    #best_stump, min_error, best_class_est = buildStump(datas_mat,labels,mat(ones((5,1))/5))
+    #print best_stump, min_error, best_class_est
+    classify_array,  aggClassEst= adaBoostTrainDS(datas_mat,labels,9)
+    print classify_array, aggClassEst
